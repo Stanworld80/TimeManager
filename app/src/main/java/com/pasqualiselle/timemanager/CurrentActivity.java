0873a26a7a6576d714f898013047da403b84756a
APasqualiSelle <alinealexandria@gmail.com>
2019-03-08 17:42:13 +0100

    Inserted Chronometer object and buttons for Start, pause and reset time

--- app/src/main/java/com/pasqualiselle/timemanager/CurrentActivity.java
+++ app/src/main/java/com/pasqualiselle/timemanager/CurrentActivity.java
@@ -1,8 +1,11 @@
 package com.pasqualiselle.timemanager;
 
 import android.content.SharedPreferences;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 public class CurrentActivity extends AppCompatActivity {
@@ -12,6 +15,13 @@ public class CurrentActivity extends AppCompatActivity {
     private SharedPreferences mSharedPreferences;
 
     private String mActivityName1;
+
+    private Chronometer mChronometer;
+    private long pauseOffset;
+    private boolean running;
+    private String mDuration;
+    private TextView mTextViewDuration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,41 @@ public class CurrentActivity extends AppCompatActivity {
         mActivityName1 = mSharedPreferences.getString(MainActivity.PREF_KEY_ACTIVITY_NAME_NUMBER1,"hey");
         mActivityNameTextView.setText(mActivityName1);
 
+        mChronometer = findViewById(R.id.chronometer);
+        mChronometer.setFormat("Time %s");
+mTextViewDuration = findViewById(R.id.textViewDuration);
+        mTextViewDuration.setText("Duration ");
+    }
 
+    public void startChronometer(View v){
+
+        if(!running){
+            mChronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            mChronometer.start();
+            running = true;
+        }
+
+    }
+
+    public Chronometer getDuration(){
+
+        return mChronometer;
+    }
+
+    public void pauseChronometer(View v){
+
+        if(running){
+            mChronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - mChronometer.getBase();
+            running = false;
+        }
+
+
+    }
+
+    public void resetChronometer(View v){
+
+         mChronometer.setBase(SystemClock.elapsedRealtime());
+         pauseOffset = 0;
     }
 }
