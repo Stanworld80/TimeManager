2f0fd779fdfbac722b6de1535712cff51cbe70c3
Stanislas SELLE <stanislas.selle@laposte.fr>
2019-03-14 19:43:23 +0100

    testing scrollview in historic activity

--- app/src/main/java/com/pasqualiselle/timemanager/HistoryActivity.java
+++ app/src/main/java/com/pasqualiselle/timemanager/HistoryActivity.java
@@ -2,6 +2,8 @@ package com.pasqualiselle.timemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        LinearLayout ll = findViewById(R.id.scrollHistoricLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int i = 0; i < 120; i++) {
+            TextView aHistoricLineTextView = new TextView(this);
+            aHistoricLineTextView.setText( "test"+":"+i );
+            ll.addView(aHistoricLineTextView, lp);
+        }
+
+
     }
 }
