7fa11e3fa99945b1aa38f1402712d79f16328f37
APasqualiSelle <alinealexandria@gmail.com>
2019-04-10 17:49:49 +0200

    In the HistoryActivity.java Put in temporary comment the previous code Implementing the beginning of the method displayDataBaseInfo() for the Activities table

--- app/src/main/java/com/pasqualiselle/timemanager/HistoryActivity.java
+++ app/src/main/java/com/pasqualiselle/timemanager/HistoryActivity.java
@@ -1,9 +1,12 @@
 package com.pasqualiselle.timemanager;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.pasqualiselle.timemanager.data.TimeManagerContract;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -12,15 +15,42 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        LinearLayout ll = findViewById(R.id.scrollHistoricLayout);
+       /* LinearLayout ll = findViewById(R.id.scrollHistoricLayout);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
         for (int i = 0; i < 120; i++) {
             TextView aHistoricLineTextView = new TextView(this);
             aHistoricLineTextView.setText( "test"+":"+i );
             ll.addView(aHistoricLineTextView, lp);
-        }
+        }*/
 
 
     }
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the ACTIVITIES database.
+     */
+
+    private void displayDatabaseInfo(){
+
+        //projection is just a name for the Columns that we were interested in getting back
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+
+                TimeManagerContract.ActivityEntry._ID,
+                TimeManagerContract.ActivityEntry.COLUMN_ACTIVITY_NAME
+
+        };
+
+        Cursor cursor = getContentResolver().query(
+                TimeManagerContract.ActivityEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
+
+    }
+
 }
